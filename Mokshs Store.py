db67053856b6dc58a77d283c0d699abdfbe8d3c1
mokshp199 <moksh.patel@ontariotechu.net>
2025-02-13 14:18:50 -0500

    Inital Commit: Created my store

---
+++ Mokshs Store.py
@@ -0,0 +1,49 @@
+print("Welcome to Mokshs Store!!")
+
+items = {
+    "mushroom": 2.50,
+    "carrot": 1.00,
+    "apple": 1.50
+}
+cart = {}
+
+def show_items():
+    print("\nItems for Sale:")
+    for item, price in items.items():
+        print(f"{item.capitalize()} - ${price:.2f}")
+
+def add_to_cart(item, quantity):
+    if item in items:
+        if item in cart:
+            cart[item] += quantity
+        else:
+            cart[item] = quantity
+        print(f"Added {quantity} {item}(s) to your cart.")
+    else:
+        print("Item not found.")
+
+def view_cart():
+    print("\nYour Cart:")
+    if not cart:
+        print("Cart is empty.")
+        return
+    total = 0
+    for item, quantity in cart.items():
+        price = items[item] * quantity
+        total += price
+        print(f"{item.capitalize()} x{quantity} - ${price:.2f}")
+    print(f"Total: ${total:.2f}")
+
+def checkout():
+    if not cart:
+        print("Your cart is empty. Nothing to checkout.")
+    else:
+        view_cart()
+        print("Thanks for shopping!")
+
+
+show_items()
+add_to_cart("mushroom", 2)
+add_to_cart("apple", 3)
+view_cart()
+checkout()
